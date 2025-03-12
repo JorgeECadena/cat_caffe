bd75a5ab95fee8a0a5fede0a4e0d62c331666aef
Jorge Cadena <a01660252@tec.mx>
2025-03-12 10:14:59 -0600

    Created a function to validate the request password with the user's password

--- back/src/lib.rs
+++ back/src/lib.rs
@@ -3,10 +3,11 @@ use dotenv::dotenv;
 use argon2::{
     password_hash::{
         rand_core::OsRng,
-        PasswordHash, PasswordHasher, SaltString, Error
+        PasswordHash, PasswordHasher, PasswordVerifier, SaltString, Error
     },
     Argon2
 };
+use crate::{db::{queries}, errors::{UserLoginError}};
 
 pub mod db;
 pub mod admin;
@@ -41,4 +42,19 @@ pub fn hash(password: &str) -> Result<String, Error> {
     let hashed_password = argon2.hash_password(password.as_bytes(), &salt)?;
 
     Ok(hashed_password.to_string())
+}
+
+pub fn check_password(user: &db::User) -> Result<(), UserLoginError> {
+    let hashed_password = queries::get_password(&user.username)?;
+
+    let parsed_hash = PasswordHash::new(&hashed_password).unwrap_or_else(|err| {
+        eprintln!("Error while parsing hash. Error: {err}");
+        process::exit(1);
+    });
+
+    if !Argon2::default().verify_password(user.password.as_bytes(), &parsed_hash).is_ok() {
+        return Err(UserLoginError::InvalidPassword);
+    }
+
+    Ok(())
 }
