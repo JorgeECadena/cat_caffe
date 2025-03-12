0b5766c2746ad8c4f4f018a0e1483f84f2c206d0
Jorge Cadena <a01660252@tec.mx>
2025-03-12 10:14:25 -0600

    Created function to get a user's password

--- back/src/db/queries.rs
+++ back/src/db/queries.rs
@@ -1,6 +1,6 @@
 use sqlite;
 use std::process;
-use crate::{db, errors::{UserCreationError}};
+use crate::{db, errors::{UserCreationError, UserLoginError}};
 
 pub fn create_user(user: &db::User) -> Result<(), UserCreationError> {
     let connection = db::open_connection();
@@ -50,4 +50,28 @@ pub fn user_exists(username: &str) -> Result<bool, UserCreationError> {
     }
 
     Ok(false)
+}
+
+pub fn get_password(username: &str) -> Result<String, UserLoginError> {
+    let connection = db::open_connection();
+
+    if !user_exists(&username).map_err(|err| {
+        match err {
+            UserCreationError::DbError(err) => UserLoginError::DbError(err),
+            _ => UserLoginError::UnknownError,
+        }
+    })? {
+        return Err(UserLoginError::UserDoesNotExist);
+    }
+
+    let query = "SELECT password FROM user WHERE username = ?";
+    let mut statement = connection.prepare(query).map_err(UserLoginError::DbError)?;
+    statement.bind((1, username)).map_err(UserLoginError::DbError)?;
+
+    if let Ok(sqlite::State::Row) = statement.next() {
+        let password: String = statement.read(0).map_err(UserLoginError::DbError)?;
+        return Ok(password);
+    }
+
+   Err(UserLoginError::UnknownError)
 }
