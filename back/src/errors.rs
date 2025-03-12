24a4a5e6d4482313a0028869f6b7d831b80df2a2
Jorge Cadena <a01660252@tec.mx>
2025-03-12 09:52:21 -0600

    Created new errors for user login

--- back/src/errors.rs
+++ back/src/errors.rs
@@ -5,3 +5,11 @@ pub enum UserCreationError {
     UserAlreadyExists,
     DbError(sqlite::Error),
 }
+
+#[derive(Debug)]
+pub enum UserLoginError {
+    UserDoesNotExist,
+    InvalidPassword,
+    UnknownError,
+    DbError(sqlite::Error),
+}
