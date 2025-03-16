64ba7c777ac9cdec8c45813535ec19178aeb0550
Jorge Cadena <a01660252@tec.mx>
2025-03-16 05:49:59 -0600

    Implemented menu listing

--- client/src/pages/admin/components/AdminHome.jsx
+++ client/src/pages/admin/components/AdminHome.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CatList from "./CatList";
+import DishList from "./DishList"
 import generics from "./Generics.module.css";
 import styles from "./Home.module.css";
 
 function AdminHome() {
     const [showCats, setShowCats] = useState(false);
+    const [showDishes, setShowDishes] = useState(false);
 
     const redirect = useNavigate();
 
-    const handleAdd = () => {
+    const handleAddCats = () => {
         redirect("/admin/create-cat");
+    };
+    const handleAddMenu = () => {
+        redirect("/admin/add-menu");
     };
 
     const handleShowCats = () => {
         setShowCats(!showCats);
+    };
+    const handleShowDishes = () => {
+        setShowDishes(!showDishes);
     };
 
     return (
@@ -28,10 +36,20 @@ function AdminHome() {
 
                         <div className={`${styles.btnsContainer}`}>
                             <button className={`${styles.button} bg-mustang vanilla fw-bold`} onClick={handleShowCats} >Ver</button>
-                            <button className={`${styles.button} bg-mustang vanilla fw-bold`} onClick={handleAdd}>Agregar</button>
+                            <button className={`${styles.button} bg-mustang vanilla fw-bold`} onClick={handleAddCats}>Agregar</button>
                         </div>
                     </div>
                     { showCats && <CatList /> }
+
+                    <div className={`${styles.container} bg-saddle`}>
+                        <h2 className={`no-margin vanilla`}>Menu</h2>
+
+                        <div className={`${styles.btnsContainer}`}>
+                            <button className={`${styles.button} bg-mustang vanilla fw-bold`} onClick={handleShowDishes}>Ver</button>
+                            <button className={`${styles.button} bg-mustang vanilla fw-bold`} onClick={handleAddMenu}>Agregar</button>
+                        </div>
+                    </div>
+                    { showDishes && <DishList />}
                 </div>
             </div>
         </>
