68c1a06f3a301d16159eccfc55e5ce98f44f6be3
Jorge Cadena <a01660252@tec.mx>
2025-03-16 03:21:32 -0600

    Added the implementation of CatList

--- client/src/pages/admin/components/AdminHome.jsx
+++ client/src/pages/admin/components/AdminHome.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import CatList from "./CatList";
 import generics from "./Generics.module.css";
 import styles from "./Home.module.css";
 
 function AdminHome() {
+    const [showCats, setShowCats] = useState(false);
+
     const redirect = useNavigate();
 
     const handleAdd = () => {
         redirect("/admin/create-cat");
+    };
+
+    const handleShowCats = () => {
+        setShowCats(!showCats);
     };
 
     return (
@@ -19,10 +27,11 @@ function AdminHome() {
                         <h2 className={`no-margin vanilla`}>Cats</h2>
 
                         <div className={`${styles.btnsContainer}`}>
-                            <button className={`${styles.button} bg-mustang vanilla fw-bold`} >Ver</button>
+                            <button className={`${styles.button} bg-mustang vanilla fw-bold`} onClick={handleShowCats} >Ver</button>
                             <button className={`${styles.button} bg-mustang vanilla fw-bold`} onClick={handleAdd}>Agregar</button>
                         </div>
                     </div>
+                    { showCats && <CatList /> }
                 </div>
             </div>
         </>
