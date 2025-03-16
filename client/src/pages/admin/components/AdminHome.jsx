e578e43b5ba0383e1784c382de290f272c13f9e8
Jorge Cadena <a01660252@tec.mx>
2025-03-15 22:05:20 -0600

    Added the view for 'cats' table

--- client/src/pages/admin/components/AdminHome.jsx
+++ client/src/pages/admin/components/AdminHome.jsx
@@ -1,10 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import generics from "./Generics.module.css";
+import styles from "./Home.module.css";
+
 function AdminHome() {
+    const redirect = useNavigate();
+
+    const handleAdd = () => {
+        redirect("/admin/create-cat");
+    };
+
     return (
         <>
-            <h1>Admin Home</h1>
+            <div className={`${generics.pageContainer}`}>
+                <div className={`${generics.container} bg-mountain`}>
+                    <h1 className={``}>Tablas</h1>
+
+                    <div className={`${styles.container} bg-saddle`}>
+                        <h2 className={`no-margin vanilla`}>Cats</h2>
+
+                        <div className={`${styles.btnsContainer}`}>
+                            <button className={`${styles.button} bg-mustang vanilla fw-bold`} >Ver</button>
+                            <button className={`${styles.button} bg-mustang vanilla fw-bold`} onClick={handleAdd}>Agregar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
-
 }
 
 export default AdminHome;
