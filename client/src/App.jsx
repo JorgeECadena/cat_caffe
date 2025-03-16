f0000382cf0d61dee6a90f76188f6099e876ace2
Jorge Cadena <a01660252@tec.mx>
2025-03-16 05:48:07 -0600

    Added new Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import AdminLogin from "./pages/admin/components/Login";
 import AdminRegister from "./pages/admin/components/Register";
 import AdminHome from "./pages/admin/components/AdminHome";
 import CatForm from "./pages/admin/components/CatForm";
+import MenuForm from "./pages/admin/components/MenuForm";
+import Login from "./pages/components/Login";
 import NotFound from "./components/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 import './App.css'
@@ -22,6 +24,12 @@ function App() {
               <CatForm />
             </ProtectedRoute>
           } />
+          <Route path="/admin/add-menu" element={
+            <ProtectedRoute>
+              <MenuForm />
+            </ProtectedRoute>
+          } />
+          <Route path="/login" element={<Login />} />
           <Route path="/admin/login" element={<AdminLogin />} />
           <Route path="/admin/create-user" element={<AdminRegister />} />
           <Route path="*" element={<NotFound />} />
