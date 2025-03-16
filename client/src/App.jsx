b873abd786de926e8ca407c85bdd8a3f00a3a476
Jorge Cadena <a01660252@tec.mx>
2025-03-15 22:04:08 -0600

    Added new protected route '/admin/create-cat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminLogin from "./pages/admin/components/Login";
 import AdminRegister from "./pages/admin/components/Register";
 import AdminHome from "./pages/admin/components/AdminHome";
+import CatForm from "./pages/admin/components/CatForm";
 import NotFound from "./components/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 import './App.css'
@@ -14,6 +15,11 @@ function App() {
           <Route path="/admin" element={
             <ProtectedRoute>
               <AdminHome />
+            </ProtectedRoute>
+          } />
+          <Route path="/admin/create-cat" element={
+            <ProtectedRoute>
+              <CatForm />
             </ProtectedRoute>
           } />
           <Route path="/admin/login" element={<AdminLogin />} />
